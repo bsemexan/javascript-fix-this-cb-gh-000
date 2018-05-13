f9507011e36abf228299512530e8e1e93f9da782
Berry  Semexan <bsemexan@gmail.com>
2018-05-13 18:38:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus = updateStatus.bind(this);
+  var updateCakeStatus = updateStatus.bind(this)
   updateCakeStatus("Prep");
   mix.call(cake, updateCakeStatus);
 }
