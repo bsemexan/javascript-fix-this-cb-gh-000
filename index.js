b6237b5fa21a822a0c2eb652028a183e629a477b
Berry  Semexan <bsemexan@gmail.com>
2018-05-13 18:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function mix(updateFunction) {
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
-  setTimeout(function() {
+  setTimeout(() => {
     this.decorate(updateFunction)
   }, 2000)
   updateFunction(status)
