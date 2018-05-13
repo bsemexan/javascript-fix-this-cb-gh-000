52675c459f094b0115e2132e2bbf793c036b8033
Berry  Semexan <bsemexan@gmail.com>
2018-05-13 18:51:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var cake = {
   decorate: function(updateFunction) {
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
     updateFunction(status)
-    setTimeout(() => {
+    setTimeout( function() {
       updateFunction(serve.apply(this, ["Happy Eating!", this.customer]))
     }, 2000)
   }
